a1f58252b19e57ca45e4bb53937d0b6d778d7d10
Stefan Ellmauthaler <stefan.ellmauthaler@gmail.com>
2021-09-25 20:25:13 +0200

    added comment

--- configuration.nix
+++ configuration.nix
@@ -88,7 +88,7 @@
   # $ nix search wget
    environment.systemPackages = with pkgs; [
      # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      emacs
+     #emacs
      aspell
      emacs-all-the-icons-fonts	
      wget
