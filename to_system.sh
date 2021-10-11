e34ea114a350ea1ef66586308760010d5d72b668
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2021-10-11 11:04:57 +0200

    fixed to_system.sh script
    Signed-off-by: Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>

--- to_system.sh
+++ to_system.sh
@@ -5,7 +5,7 @@
 cp configuration.nix /etc/nixos/.
 cp -r programs /etc/nixos/.
 mkdir -p /etc/nixos/machine
-for machine in `exa --all -l machine | grep "^d" | cut -d" " -f8`; do
+for machine in `ls -al machine | grep "^d" | rev | cut -d" " -f1 | rev`; do
     cp -r machine/$machine /etc/nixos/machine/.
 done
 
