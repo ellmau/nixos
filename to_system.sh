434324e584149cc340f513150d098ef114225959
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2021-10-11 10:55:17 +0200

    2nd script and setup for work monitors
    Signed-off-by: Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>

--- to_system.sh
+++ to_system.sh
@@ -9,4 +9,4 @@ for machine in `exa --all -l machine | grep "^d" | cut -d" " -f8`; do
     cp -r machine/$machine /etc/nixos/machine/.
 done
 
-cp -r ./home/config/* $HOME/.config/.
+cp -r ./home/config/* /home/ellmau/.config/.
