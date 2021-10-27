ebd7d33fd52bdbe1cefaaefc9163dbf86e2f2d14
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2021-10-28 00:17:42 +0200

    nix-shell zsh plugin
    Signed-off-by: Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>

--- users/ellmau/zsh.nix
+++ users/ellmau/zsh.nix
@@ -26,6 +26,18 @@
         lal = "ll --all";
         lla = "ll --all";
       };
+      plugins = [
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.4.0";
+            sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
+          };
+        }
+      ];
     };
     
     starship = {
