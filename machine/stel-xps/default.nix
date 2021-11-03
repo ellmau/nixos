a0b620cfadda18da28cc3cea866b70d1311aeec6
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2021-11-03 23:03:45 +0100

    fixed stel-xps issues
    Signed-off-by: Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>

---
+++ machine/stel-xps/default.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ...}:
+{ 
+  networking.hostName = "stel-xps"; # define the hostname
+
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+  ];
+
+  services.autorandr.enable = true;
+  services.xserver.desktopManager.wallpaper.mode = "fill";
+}
