300c4ee9ba490d8d0987775e67d37953a31cca5c
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2021-12-01 09:27:59 +0100

    HOME updated on new version

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-21.05";
+      url = "github:nix-community/home-manager/release-21.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     
