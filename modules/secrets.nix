12b4c4c01385c66978f9970abb920ec571381202
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2022-05-31 16:22:47 +0200

    Add option for sshKeyPaths in sops

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -12,6 +12,10 @@ with lib; {
         age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
         age.keyFile = "/var/lib/sops-nix/key.txt";
         age.generateKey = true;
+        gnupg.sshKeyPaths = [
+          ../secrets/keys/users
+          ../secrets/keys/hosts
+        ];
       };
     };
 }
