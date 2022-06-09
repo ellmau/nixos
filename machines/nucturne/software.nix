982acf12613b1c4d317d2f77bcfa3c87e66b4316
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2022-06-09 13:35:05 +0200

    Add nucturne configuration

---
+++ machines/nucturne/software.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    libreoffice-fresh
+  ];
+
+  programs = {
+    java.enable = true;
+  };
+
+  services = {
+    autorandr.enable = false;
+  };
+
+  elss = {
+    programs = {
+      aspell.enable = true;
+      # Enable communication programs
+      communication.enable = true;
+      emacs.enable = true;
+      obsstudio.enable = true;
+      python.enable = true;
+    };
+
+    texlive.enable = true;
+    steam-run.enable = true;
+  };
+}
