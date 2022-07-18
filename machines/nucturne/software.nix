2f46b6bc3fe7bd6dddb788a84132680b0a25bc2e
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2022-07-18 10:25:45 +0200

    Add onlyoffice to the installed office packages

--- machines/nucturne/software.nix
+++ machines/nucturne/software.nix
@@ -2,6 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     libreoffice-fresh
+    onlyoffice-bin
     krita # drawing tool for my wacom
   ];
 
--- machines/stel-xps/software.nix
+++ machines/stel-xps/software.nix
@@ -3,6 +3,7 @@
   environment.systemPackages = with pkgs; [
     brightnessctl
     libreoffice-fresh
+    onlyoffice-bin
   ];
 
   programs = {
