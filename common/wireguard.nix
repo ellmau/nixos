706628e5d5a2767766886396270bd523ac32e567
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2022-07-31 15:03:08 +0200

    Add metis secrets

--- common/wireguard.nix
+++ common/wireguard.nix
@@ -8,13 +8,13 @@ with lib; {
           metis = {
             localIp = "1";
             extraIps = [ "142" ];
-            publicKey = ""; #TODO
+            publicKey = "wP49t1TYXI3ucsYb8RavNGwIf+8nx5UBgDU0PM9VlnI=";
             endpoint = "metis.ellmauthaler.net:51820"; #TODO
           };
         };
 
         peers = { # TODO
-          stel = {
+          stelphone = {
             localIp = "142";
             publicKey = "6ZwilfrS1J/dMYRnwIMcQ3cW0KtJdLRj5VnSOjwOpn8=";
           };
