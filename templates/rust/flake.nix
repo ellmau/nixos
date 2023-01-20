2e87b3e5661bfd6e550a53acb0be0498beebad89
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2023-01-20 13:15:58 +0100

    Update flake templates to 22.11

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -2,7 +2,7 @@
   description = "basic rust flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
