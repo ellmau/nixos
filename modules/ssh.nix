24b3d61e482d921c334de34dd7443e28fc9802ee
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2023-05-31 17:02:01 +0200

    Upgrade to nixos 23.05

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,20 +1,15 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 with lib; {
   options.elss.sshd.enable = mkEnableOption "Set up sshd";
 
-  config = let
-    cfg = config.elss.sshd;
-  in
-    mkIf cfg.enable {
-      services.openssh = {
-        enable = true;
-        passwordAuthentication = false;
-        permitRootLogin = "no";
+  config = let cfg = config.elss.sshd;
+  in mkIf cfg.enable {
+    services.openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
       };
     };
+  };
 }
