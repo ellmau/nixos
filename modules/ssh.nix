f265f9a50e12c6296f688300c3375f86a98a2728
Stefan Ellmauthaler <stefan.ellmauthaler@tu-dresden.de>
2023-06-01 14:45:35 +0200

    nix fmt

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,15 +1,22 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 with lib; {
   options.elss.sshd.enable = mkEnableOption "Set up sshd";
 
-  config = let cfg = config.elss.sshd;
-  in mkIf cfg.enable {
-    services.openssh = {
-      enable = true;
-      settings = {
-        PasswordAuthentication = false;
-        PermitRootLogin = "no";
+  config = let
+    cfg = config.elss.sshd;
+  in
+    mkIf cfg.enable {
+      services.openssh = {
+        enable = true;
+        settings = {
+          PasswordAuthentication = false;
+          PermitRootLogin = "no";
+        };
       };
     };
-  };
 }
--- secrets/shell.nix
+++ secrets/shell.nix
@@ -1,16 +1,20 @@
-{ pkgs ? import <nixpkgs> { }, sops-nix ? pkgs.callPackage <sops-nix> { }
-, name ? "secrets", ... }:
-let
+{
+  pkgs ? import <nixpkgs> {},
+  sops-nix ? pkgs.callPackage <sops-nix> {},
+  name ? "secrets",
+  ...
+}: let
   sops-rekey = pkgs.writeShellScriptBin "sops-rekey" ''
     ${pkgs.findutils}/bin/find . -wholename '*/secrets/*.yaml' -exec ${pkgs.sops}/bin/sops updatekeys {} \;
   '';
-in pkgs.mkShell {
-  sopsPGPKeyDirs = [ ./keys/users ./keys/hosts ];
-  name = name;
-  nativeBuildInputs = [
-    sops-nix.sops-import-keys-hook
-    sops-nix.ssh-to-pgp
-    sops-rekey
-    pkgs.wireguard-tools
-  ];
-}
+in
+  pkgs.mkShell {
+    sopsPGPKeyDirs = [./keys/users ./keys/hosts];
+    name = name;
+    nativeBuildInputs = [
+      sops-nix.sops-import-keys-hook
+      sops-nix.ssh-to-pgp
+      sops-rekey
+      pkgs.wireguard-tools
+    ];
+  }
