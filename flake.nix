5931e932cdb6d0255d23f3e8f4c334bb2f6a91f3
Stefan Ellmauthaler <stefan.ellmauthaler@gmail.com>
2024-08-22 09:50:51 +0200

    Use lix instead of nix
    Signed-off-by: Stefan Ellmauthaler <stefan.ellmauthaler@gmail.com>

--- flake.nix
+++ flake.nix
@@ -11,6 +11,17 @@
     #  inputs.nixpkgs.follows = "nixpkgs";
     #};
 
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
+
     nixos-hardware = {url = "github:NixOS/nixos-hardware/master";};
 
     home-manager = {
@@ -123,6 +134,7 @@
             inputs.simple-nixos-mailserver.nixosModules.mailserver
             ./common/wireguard.nix
             inputs.glpi-inventory.nixosModules.glpi-inventory
+            inputs.lix-module.nixosModules.default
           ]
           ++ (map (name: ./modules + "/${name}") (moduleNames ./modules));
         specialArgs = {
