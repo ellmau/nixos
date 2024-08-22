9c497f51a02b464ead0d2f27d8b003fc8f4603fe
Stefan Ellmauthaler <stefan.ellmauthaler@gmail.com>
2024-08-22 10:36:12 +0200

    Use release version of lix instead of daily main updates
    Signed-off-by: Stefan Ellmauthaler <stefan.ellmauthaler@gmail.com>

--- flake.nix
+++ flake.nix
@@ -11,15 +11,9 @@
     #  inputs.nixpkgs.follows = "nixpkgs";
     #};
 
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
     };
 
     nixos-hardware = {url = "github:NixOS/nixos-hardware/master";};
