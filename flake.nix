58bbd04f76fd749830786a816d9d0fa4b7f8d4bf
Stefan Ellmauthaler <stefan.ellmauthaler@gmail.com>
2025-04-23 14:25:03 +0200

    [lix]update
    Signed-off-by: Stefan Ellmauthaler <stefan.ellmauthaler@gmail.com>

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     #};
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
